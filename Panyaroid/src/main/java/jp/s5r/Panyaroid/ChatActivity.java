287eef21b4e09baa43d8ac2f508876c1454a037d
shunirr <shunirr@gmail.com>
2013-03-08 20:28:33 +0900

    チャット画面の View を追加

--- Panyaroid/src/main/java/jp/s5r/Panyaroid/ChatActivity.java
+++ Panyaroid/src/main/java/jp/s5r/Panyaroid/ChatActivity.java
@@ -1,11 +1,13 @@
 package jp.s5r.Panyaroid;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class ChatActivity extends AbstractBaseActivity {
   @Override
   protected void onCreate(final Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
+    getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+    setContentView(R.layout.chat);
   }
 }
