c89ea67bfead31a87ec24f5df786abbe2286eb71
shunirr <shunirr@gmail.com>
2013-03-11 20:52:16 +0900

    Application クラスの追加

---
+++ src/main/java/jp/s5r/client/pannya/PanyaroidApplication.java
@@ -0,0 +1,13 @@
+package jp.s5r.client.pannya;
+
+import android.app.Application;
+
+public class PanyaroidApplication extends Application {
+  @Override
+  public void onCreate() {
+  }
+
+  @Override
+  public void onTerminate() {
+  }
+}
